3881c5a9851b6a7e11c485b4f9827399926bf368
pepperdad <jeong20909@naver.com>
2022-01-02 23:02:07 +0900

    second commit

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const bodyParser = require('body-parser');
+const { User } = require('./models/User.js');
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose');
+
 mongoose
   .connect(
     'mongodb+srv://jeong20909:aa^^ehdud12!@boilerplate.zpmr1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
@@ -12,6 +21,18 @@ mongoose
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+// register를 위한 route
+app.post('/register', (req, res) => {
+  // 회원가입 할 때 필요한 정보들을 client에서 가져오면
+  // 그것들을 데이터베이스에 넣어준다.
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({ success: true });
+  });
 });
 
 app.listen(port, () => {
